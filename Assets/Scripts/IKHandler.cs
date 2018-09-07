7b054ea25dbf40d62122ed74e9abdca99073f38b
Halvor Bakken Smedås <halvorbs@stud.ntnu.no>
2018-09-08 01:33:51 +0200

    Added simple IK, realized I could start just doing IK on hands

---
+++ Assets/Scripts/IKHandler.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IKHandler : MonoBehaviour
+{
+
+    Animator animator;
+
+    public float IK_weight = 1;
+
+    public Transform root;
+
+    public Transform IK_targetLeftFoot;
+    public Transform IK_targetRightFoot;
+
+    public Transform IK_hintLeftKnee;
+    public Transform IK_hintRightKnee;
+
+    public Transform IK_targetLeftHand;
+    public Transform IK_targetRightHand;
+
+    public Transform IK_head;
+
+
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+
+    }
+
+    private void OnAnimatorIK( int layerIndex )
+    {
+
+        //HANDS
+        animator.SetIKPositionWeight(AvatarIKGoal.LeftHand, IK_weight);
+        animator.SetIKPositionWeight(AvatarIKGoal.RightHand, IK_weight);
+
+        animator.SetIKPosition(AvatarIKGoal.LeftHand, IK_targetLeftHand.position);
+        animator.SetIKPosition(AvatarIKGoal.RightHand, IK_targetRightHand.position);
+
+        //TODO: Add Elbows
+        
+        //FEET
+        animator.SetIKPositionWeight(AvatarIKGoal.LeftFoot, IK_weight);
+        animator.SetIKPositionWeight(AvatarIKGoal.RightFoot, IK_weight);
+
+        animator.SetIKPosition(AvatarIKGoal.LeftFoot, IK_targetLeftFoot.position);
+        animator.SetIKPosition(AvatarIKGoal.RightFoot, IK_targetRightFoot.position);
+
+        //KNEES
+        animator.SetIKHintPositionWeight(AvatarIKHint.LeftKnee, IK_weight);
+        animator.SetIKHintPositionWeight(AvatarIKHint.RightKnee, IK_weight);
+
+        animator.SetIKHintPosition(AvatarIKHint.LeftKnee, IK_hintLeftKnee.position);
+        animator.SetIKHintPosition(AvatarIKHint.RightKnee, IK_hintRightKnee.position);
+
+        //MORE?
+
+        //animator.MatchTarget()
+        //animator.MatchTarget(IK_head.position, IK_head.rotation, AvatarTarget.)
+
+        animator.SetLookAtPosition(IK_head.position + IK_head.forward);
+        animator.SetLookAtWeight(1);
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        if (IK_targetLeftFoot != null && IK_hintLeftKnee != null && root != null)
+        {
+            Gizmos.DrawLine(IK_hintLeftKnee.position, IK_targetLeftFoot.position);
+            Gizmos.DrawLine(IK_hintLeftKnee.position, root.position);
+        }
+        if (IK_targetRightFoot != null && IK_hintRightKnee != null && root != null)
+        {
+            Gizmos.DrawLine(IK_hintRightKnee.position, IK_targetRightFoot.position);
+            Gizmos.DrawLine(IK_hintRightKnee.position, root.position);
+        }
+
+        if (IK_targetLeftHand != null && root != null)
+        {
+            Gizmos.DrawLine(IK_targetLeftHand.position, root.position);
+        }
+        if (IK_targetRightHand != null && root != null)
+        {
+            Gizmos.DrawLine(IK_targetRightHand.position, root.position);
+        }
+
+    }
+}
